1c71abe1748f9b009200007db78a892d4eee4be4
mja00 <teamnirvana2@gmail.com>
2017-08-10 01:33:40 -0400

    Initial Commit

---
+++ WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,263 @@
+﻿Imports System
+Imports System.IO
+Class MainWindow
+    Private Sub myButton_Click(sender As Object, e As RoutedEventArgs) Handles myButton.Click
+        Dim myFirstForm As New MainWindow
+        Dim mySecondForm As New Window1
+        myFirstForm.Hide()
+        mySecondForm.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub _1GUnit1_Click(sender As Object, e As RoutedEventArgs)
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/1GUnit1 best")
+    End Sub
+
+    Private Sub AbdulHD_Click(sender As Object, e As RoutedEventArgs) Handles AbdulHD.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/AbdulHD best")
+    End Sub
+
+    Private Sub AgentNorth_Click(sender As Object, e As RoutedEventArgs) Handles AgentNorth.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/AgentNorth best")
+    End Sub
+
+    Private Sub alexusnightingale_Click(sender As Object, e As RoutedEventArgs) Handles alexusnightingale.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/alexusnightingale best")
+    End Sub
+
+    Private Sub AndyMilonakis_Click(sender As Object, e As RoutedEventArgs) Handles AndyMilonakis.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/AndyMilonakis best")
+    End Sub
+
+    Private Sub anthonyz__Click(sender As Object, e As RoutedEventArgs) Handles anthonyz_.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/anthonyz_ best")
+    End Sub
+
+    Private Sub BFLY003_Click(sender As Object, e As RoutedEventArgs) Handles BFLY003.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/BFLY003 best")
+    End Sub
+
+    Private Sub boscoreli_gaming_Click(sender As Object, e As RoutedEventArgs) Handles boscoreli_gaming.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/boscoreli_gaming best")
+    End Sub
+
+    Private Sub BPZ_Click(sender As Object, e As RoutedEventArgs) Handles BPZ.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/BPZ_Gaming best")
+    End Sub
+
+    Private Sub BuckkNaked_Click(sender As Object, e As RoutedEventArgs) Handles BuckkNaked.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/BuckkNaked best")
+    End Sub
+
+    Private Sub BurkeBlack_Click(sender As Object, e As RoutedEventArgs) Handles BurkeBlack.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/BurkeBlack best")
+    End Sub
+
+    Private Sub Ch1ckenb0ne_Click(sender As Object, e As RoutedEventArgs) Handles Ch1ckenb0ne.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Ch1ckenb0ne best")
+    End Sub
+
+    Private Sub ChiefDas_Click(sender As Object, e As RoutedEventArgs) Handles ChiefDas.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/ChiefDas best")
+    End Sub
+
+    Private Sub ClassyPax_Click(sender As Object, e As RoutedEventArgs) Handles ClassyPax.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/ClassyPax best")
+    End Sub
+
+    Private Sub CletusBueford_Click(sender As Object, e As RoutedEventArgs) Handles CletusBueford.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/CletusBueford best")
+    End Sub
+
+    Private Sub CoolidgeHD_Click(sender As Object, e As RoutedEventArgs) Handles CoolidgeHD.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/CoolidgeHD best")
+    End Sub
+
+    Private Sub dasMehdi_Click(sender As Object, e As RoutedEventArgs) Handles dasMehdi.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/dasMehdi best")
+    End Sub
+
+    Private Sub DisbeArex_Click(sender As Object, e As RoutedEventArgs) Handles DisbeArex.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/DisbeArex best")
+    End Sub
+
+    Private Sub djkwix_Click(sender As Object, e As RoutedEventArgs) Handles djkwix.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/djkwix best")
+    End Sub
+
+    Private Sub DrunkBabyYoshi_Click(sender As Object, e As RoutedEventArgs) Handles DrunkBabyYoshi.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/DrunkBabyYoshi best")
+    End Sub
+
+    Private Sub Esoluu_Click(sender As Object, e As RoutedEventArgs) Handles Esoluu.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Esoluu best")
+    End Sub
+
+    Private Sub Finkone_Click(sender As Object, e As RoutedEventArgs) Handles Finkone.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/FinKone best")
+    End Sub
+
+    Private Sub FistofTheWalrus_Click(sender As Object, e As RoutedEventArgs) Handles FistofTheWalrus.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/FistofTheWalrus best")
+    End Sub
+
+    Private Sub Five0AnthO_Click(sender As Object, e As RoutedEventArgs) Handles Five0AnthO.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Five0AnthO best")
+    End Sub
+
+    Private Sub GeeFamous92_Click(sender As Object, e As RoutedEventArgs) Handles GeeFamous92.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/GeeFamous92 best")
+    End Sub
+
+    Private Sub GhilleGuyTV_Click(sender As Object, e As RoutedEventArgs) Handles GhilleGuyTV.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/GhilleGuyTV best")
+    End Sub
+
+    Private Sub GiantFaffle_Click(sender As Object, e As RoutedEventArgs) Handles GiantFaffle.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/GiantFaffle best")
+    End Sub
+
+    Private Sub GloryD_Click(sender As Object, e As RoutedEventArgs) Handles GloryD.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/GloryD best")
+    End Sub
+
+    Private Sub Harryow_Click(sender As Object, e As RoutedEventArgs) Handles Harryow.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Harryow best")
+    End Sub
+
+    Private Sub Hazard_Click(sender As Object, e As RoutedEventArgs) Handles Hazard.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Hazard best")
+    End Sub
+
+    Private Sub Hirona_Click(sender As Object, e As RoutedEventArgs) Handles Hirona.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Hirona best")
+    End Sub
+
+    Private Sub IAmSp00n_Click(sender As Object, e As RoutedEventArgs) Handles IAmSp00n.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/IAmSp00n best")
+    End Sub
+
+    Private Sub ImmortalLive_Click(sender As Object, e As RoutedEventArgs) Handles ImmortalLive.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/ImmortalLive best")
+    End Sub
+
+    Private Sub ironmonkeytv_Click(sender As Object, e As RoutedEventArgs) Handles ironmonkeytv.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/ironmonkeytv best")
+    End Sub
+
+    Private Sub itzButch_Click(sender As Object, e As RoutedEventArgs) Handles itzButch.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/itzButch best")
+    End Sub
+
+    Private Sub J_Reed_Click(sender As Object, e As RoutedEventArgs) Handles J_Reed.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/J_Reed best")
+    End Sub
+
+    Private Sub JaboodyShow_Click(sender As Object, e As RoutedEventArgs) Handles JaboodyShow.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/JaboodyShow best")
+    End Sub
+
+    Private Sub jake_terek_Click(sender As Object, e As RoutedEventArgs) Handles jake_terek.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/jake_terek best")
+    End Sub
+
+    Private Sub JmxTwiztid_Click(sender As Object, e As RoutedEventArgs) Handles JmxTwiztid.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/JmxTwiztid best")
+    End Sub
+
+    Private Sub John782_Click(sender As Object, e As RoutedEventArgs) Handles John782.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/John782 best")
+    End Sub
+
+    Private Sub Jolly__Jess_Click(sender As Object, e As RoutedEventArgs) Handles Jolly__Jess.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Jolly__Jess best")
+    End Sub
+
+    Private Sub JTtroy_Click(sender As Object, e As RoutedEventArgs) Handles JTtroy.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/JTtroy best")
+    End Sub
+
+    Private Sub kaceytron_Click(sender As Object, e As RoutedEventArgs) Handles kaceytron.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/kaceytron best")
+    End Sub
+
+    Private Sub KaptainKpress_Click(sender As Object, e As RoutedEventArgs) Handles KaptainKpress.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/KaptainKpress best")
+    End Sub
+
+    Private Sub KatieRouu_Click(sender As Object, e As RoutedEventArgs) Handles KatieRouu.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/KatieRouu best")
+    End Sub
+
+    Private Sub Kithicor27_Click(sender As Object, e As RoutedEventArgs) Handles Kithicor27.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Kithicor27 best")
+    End Sub
+
+    Private Sub LarryX7_Click(sender As Object, e As RoutedEventArgs) Handles LarryX7.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/LarryX7 best")
+    End Sub
+
+    Private Sub lawdog1096_Click(sender As Object, e As RoutedEventArgs) Handles lawdog1096.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/lawdog1096 best")
+    End Sub
+
+    Private Sub LiftYourGame_Click(sender As Object, e As RoutedEventArgs) Handles LiftYourGame.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/LifeYourGame best")
+    End Sub
+
+    Private Sub loveANF3L_Click(sender As Object, e As RoutedEventArgs) Handles loveANF3L.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/loveANG3L best")
+    End Sub
+
+    Private Sub mart1n_g_Click(sender As Object, e As RoutedEventArgs) Handles mart1n_g.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/mart1n_g best")
+    End Sub
+
+    Private Sub MiltonTPike1_Click(sender As Object, e As RoutedEventArgs) Handles MiltonTPike1.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/MiltonTPike1 best")
+    End Sub
+
+    Private Sub MintsTV_Click(sender As Object, e As RoutedEventArgs) Handles MintsTV.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/MintsTV best")
+    End Sub
+
+    Private Sub MrMoonsHouse_Click(sender As Object, e As RoutedEventArgs) Handles MrMoonsHouse.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/MrMoonsHouse best")
+    End Sub
+
+    Private Sub MsDeathRabbit_Click(sender As Object, e As RoutedEventArgs) Handles MsDeathRabbit.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/MsDeathRabbit best")
+    End Sub
+
+    Private Sub Og_BluePill_Click(sender As Object, e As RoutedEventArgs) Handles Og_BluePill.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Og_BluePill best")
+    End Sub
+
+    Private Sub PENTAhearth_Click(sender As Object, e As RoutedEventArgs) Handles PENTAhearth.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/PENTAhearth best")
+    End Sub
+
+    Private Sub Pinky_Click(sender As Object, e As RoutedEventArgs) Handles Pinky.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Pinky best")
+    End Sub
+
+    Private Sub PmsProxy_Click(sender As Object, e As RoutedEventArgs) Handles PmsProxy.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Pmsproxy best")
+    End Sub
+
+    Private Sub PorkMarshmallow_Click(sender As Object, e As RoutedEventArgs) Handles PorkMarshmallow.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/PorkMarshmallow best")
+    End Sub
+
+    Private Sub PrimusPalus_Click(sender As Object, e As RoutedEventArgs) Handles PrimusPalus.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/PrimusPalus best")
+    End Sub
+
+    Private Sub Purrluna_Click(sender As Object, e As RoutedEventArgs) Handles Purrluna.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Purrluna best")
+    End Sub
+
+    Private Sub Pydrex_Click(sender As Object, e As RoutedEventArgs) Handles Pydrex.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Pydrex best")
+    End Sub
+End Class
---
+++ WpfApp1/Window1.xaml.vb
@@ -0,0 +1,145 @@
+﻿Public Class Window1
+    Private Sub BackButton_Click(sender As Object, e As RoutedEventArgs) Handles BackButton.Click
+        Dim myFirstForm As New MainWindow
+        Dim mySecondForm As New Window1
+        myFirstForm.Hide()
+        mySecondForm.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Ssaab_Click(sender As Object, e As RoutedEventArgs)
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/ssaab best")
+    End Sub
+
+    Private Sub RajjPatel_Click(sender As Object, e As RoutedEventArgs) Handles RajjPatel.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/RajjPatel best")
+    End Sub
+
+    Private Sub Rastafied_Click(sender As Object, e As RoutedEventArgs) Handles Rastafied.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Rastafied best")
+    End Sub
+
+    Private Sub RastaRafikii_Click(sender As Object, e As RoutedEventArgs) Handles RastaRafikii.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/RastaRafikii best")
+    End Sub
+
+    Private Sub SAMZHIT_Click(sender As Object, e As RoutedEventArgs) Handles SAMZHIT.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/SAMZHIT best")
+    End Sub
+
+    Private Sub Sarapocalypse_Click(sender As Object, e As RoutedEventArgs) Handles Sarapocalypse.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Sarapocalypse best")
+    End Sub
+
+    Private Sub SayeedBlack_Click(sender As Object, e As RoutedEventArgs) Handles SayeedBlack.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/SayeedBlack best")
+    End Sub
+
+    Private Sub SeanKingV_Click(sender As Object, e As RoutedEventArgs) Handles SeanKingV.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/SeanKingV best")
+    End Sub
+
+    Private Sub Selvek_Click(sender As Object, e As RoutedEventArgs) Handles Selvek.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Selvek best")
+    End Sub
+
+    Private Sub ShaZ_Click(sender As Object, e As RoutedEventArgs) Handles ShaZ.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/ShaZ best")
+    End Sub
+
+    Private Sub SheriffEli_Click(sender As Object, e As RoutedEventArgs) Handles SheriffEli.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/SheriifEli best")
+    End Sub
+
+    Private Sub SHPTV_Click(sender As Object, e As RoutedEventArgs) Handles SHPTV.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/SHP_TV best")
+    End Sub
+
+    Private Sub SilentSentry_Click(sender As Object, e As RoutedEventArgs) Handles SilentSentry.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/SilentSentry best")
+    End Sub
+
+    Private Sub SimplyJulian_Click(sender As Object, e As RoutedEventArgs) Handles SimplyJulian.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/SimplyJulian best")
+    End Sub
+
+    Private Sub SirCoffeestain_Click(sender As Object, e As RoutedEventArgs) Handles SirCoffeestain.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/SirCoffeestain best")
+    End Sub
+
+    Private Sub SirPinkleton00_Click(sender As Object, e As RoutedEventArgs) Handles SirPinkleton00.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/SirPinkleton00 best")
+    End Sub
+
+    Private Sub SkipGently_Click(sender As Object, e As RoutedEventArgs) Handles SkipGently.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/SkipGently best")
+    End Sub
+
+    Private Sub SomeTexas_Dude_Click(sender As Object, e As RoutedEventArgs) Handles SomeTexas_Dude.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Some_Texas_Dude best")
+    End Sub
+
+    Private Sub TastyTV_Click(sender As Object, e As RoutedEventArgs) Handles TastyTV.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/TastyTV best")
+    End Sub
+
+    Private Sub Thadrius_Click(sender As Object, e As RoutedEventArgs) Handles Thadrius.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Thadrius best")
+    End Sub
+
+    Private Sub TheBuddha3_Click(sender As Object, e As RoutedEventArgs) Handles TheBuddha3.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/TheBuddha3 best")
+    End Sub
+
+    Private Sub thelawmangaming_Click(sender As Object, e As RoutedEventArgs) Handles thelawmangaming.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/thelawmangaming best")
+    End Sub
+
+    Private Sub TigerWriter_Click(sender As Object, e As RoutedEventArgs) Handles TigerWriter.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/TigerWriter best")
+    End Sub
+
+    Private Sub Timmac_Click(sender As Object, e As RoutedEventArgs) Handles Timmac.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Timmac best")
+    End Sub
+
+    Private Sub TVSBOH_Click(sender As Object, e As RoutedEventArgs) Handles TVSBOH.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/TVSBOH best")
+    End Sub
+
+    Private Sub UndeadBaron_Click(sender As Object, e As RoutedEventArgs) Handles UndeadBaron.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/UndeadBaron best")
+    End Sub
+
+    Private Sub ValkyrieSpirit_Click(sender As Object, e As RoutedEventArgs) Handles ValkyrieSpirit.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/ValkyrieSpirit best")
+    End Sub
+
+    Private Sub Will_ko_Click(sender As Object, e As RoutedEventArgs) Handles Will_ko.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Will_ko best")
+    End Sub
+
+    Private Sub willitkimchi_Click(sender As Object, e As RoutedEventArgs) Handles willitkimchi.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Willitkimchi best")
+    End Sub
+
+    Private Sub Wish_Click(sender As Object, e As RoutedEventArgs) Handles Wish.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Wish best")
+    End Sub
+
+    Private Sub X_xZimx_X_Click(sender As Object, e As RoutedEventArgs) Handles X_xZimx_X.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/X_xZimx_X best")
+    End Sub
+
+    Private Sub Xiceman_Click(sender As Object, e As RoutedEventArgs) Handles Xiceman.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Xiceman best")
+    End Sub
+
+    Private Sub XProph3cyx_Click(sender As Object, e As RoutedEventArgs) Handles XProph3cyx.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/XProph3cyx best")
+    End Sub
+
+    Private Sub Zaquelle_Click(sender As Object, e As RoutedEventArgs) Handles Zaquelle.Click
+        Process.Start("CMD", "/C data\streamlink.exe http://twitch.tv/Zaquelle best")
+    End Sub
+End Class
